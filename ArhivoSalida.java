92e7ef661330e1a5ad4f6c4d04861be74b54c5a3
marceloleiton1997 <marcelo.leiton@alumnos.uv.cl>
2018-12-20 20:28:20 -0300

    1.0.5

--- ArhivoSalida.java
+++ ArhivoSalida.java
@@ -1,6 +1,17 @@
-
 package ProyectoHotel;
 
 public class ArhivoSalida {
+    
+    public void reservaciones(){
+        
+    }
+    
+    public void contabilidad(){
+        
+    }
+     
+    public void reportes(){
+        
+    }
 
 }
