b01f7dff08a482092cf4e91e3e1a5e588fddbd2c
csr38 <cesar.tapia@alumnos.uv.cl>
2018-12-20 20:35:51 -0300

    1.0.6

--- ArhivoSalida.java
+++ ArhivoSalida.java
@@ -1,9 +1,27 @@
 package ProyectoHotel;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class ArhivoSalida {
     
     public void reservaciones(){
         
+        String cadena="";
+        try {
+            FileReader lector = new FileReader("operaciones.in");
+            BufferedReader BR = new BufferedReader(lector);
+            while ((cadena = BR.readLine()) != null) {
+                
+                System.out.println(cadena);
+            }
+
+            BR.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+
     }
     
     public void contabilidad(){
