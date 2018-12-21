be7715176b62e282d3d90487fbc0e2c49f0918b6
marceloleiton1997 <marcelo.leiton@alumnos.uv.cl>
2018-12-21 01:46:34 -0300

    1.1.1

--- main.java
+++ main.java
@@ -6,10 +6,15 @@ import java.io.IOException;
 public class main {
     public static void main(String[] args) throws IOException {
         ArchivoEntrada a = new ArchivoEntrada();
-        System.out.println("\n ------------  INICIALIZAR.IN ------------ ");
-        a.inicializar();
-        System.out.println(" \n ------------ PRECIOS.IN ------------  ");
-        a.precios();
+        //System.out.println("\n ------------  INICIALIZAR.IN ------------ ");
+        //a.inicializar();
+        //System.out.println(" \n ------------ PRECIOS.IN ------------  ");
+        //a.precios();
+         //System.out.println(" \n ------------ OPERACIONES.IN ------------  ");
+         //a.operaciones();
+         ArhivoSalida s= new ArhivoSalida();
+         s.contabilidad();
+         //s.reservaciones();
     }
     
 }
